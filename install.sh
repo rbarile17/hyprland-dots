acd4415f79ab4bb3fd3c47360fa6276810807360
Roberto Barile <roberto.barile98@gmail.com>
2023-01-30 10:11:40 +0100

    cambiato path yay

--- install.sh
+++ install.sh
@@ -4,12 +4,12 @@ sudo pacman -S brightnessctl exa foot nemo ttf-jetbrains-mono otf-font-awesome \
     gammastep evince mpv htop playerctl git base-devel \
     --noconfirm
 
-cd /opt
 sudo git clone https://aur.archlinux.org/yay-git.git
 sudo chown -R roberto:roberto ./yay-git
 cd yay-git
 makepkg -si
-cd /home/roberto
+cd ..
+sudo rm -rf yay-git
 
 yay --noprovides --answerdiff None --answerclean None --mflags "--noconfirm --nocheck" -S \
     hyprland-git rofi-lbonn-wayland wob waybar-hyprland-git apostrophe \
