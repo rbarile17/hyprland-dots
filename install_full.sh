1bbeb50cbf25efad4cd4223c63b6ffaf3b93cb3e
Roberto Barile <roberto.barile98@gmail.com>
2023-01-30 10:06:47 +0100

    rimosso pipewire-pulse

--- install_full.sh
+++ install_full.sh
@@ -1,10 +1,9 @@
 ./install.sh
 
 yay --noprovides --answerdiff None --answerclean None --mflags "--noconfirm --nocheck" -S \
-    sddm-git bluez bleuman pipewire-pulse pavucontrol    
+    sddm-git bluez bleuman pavucontrol    
 
 sudo systemctl enable sddm.service
 sudo systemctl enable bluetooth
-systemctl --user enable pipewire-pulse
 
 yay -S brave-bin visual-studio-code-bin bitwarden authy logseq-desktop-bin insync
