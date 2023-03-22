f3e4b6668f4540b532d8225615bf4066656167d3
Roberto Barile <r.barile17@studenti.uniba.it>
2023-03-22 13:25:15 -0100

    create screenshots directory

--- override_config.sh
+++ override_config.sh
@@ -4,6 +4,8 @@ sudo cp -r ./home/pics ~
 sudo cp ./.bashrc ~
 sudo cp ./etc/environment /etc/environment
 
+sudo mkdir -p ~/pics/screenshots
+
 sudo chmod -R a+rwx ~/scripts
 sudo chmod a+x ~/.config/hypr/autostart
 sudo chmod -R a+rw ~/.config
